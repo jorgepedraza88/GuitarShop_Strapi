f2fe14222311482125b54ec6a31934657aa47591
Jorge Pedraza <jorgepedraza1988@gmail.com>
2022-03-02 17:43:18 +0100

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ed4c824e88d07ab3233ab169f6efc0ac'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd5c660aa-ca4f-4f2c-a6bd-8dc2379ece52'
+};
